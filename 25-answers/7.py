15ad103f934bd5436e4700ab28a23bec9ec386bc
Maharaja <maharaja7251@gmail.com>
2025-06-04 11:38:48 +0000

    latest

--- 25-answers/7.py
+++ 25-answers/7.py
@@ -1,4 +1,8 @@
 # 7. Use filter to select words from a list that are palindromes.
-palindromes = lambda words: list(filter(lambda w: w == w[::-1], words))
+#    Input: ["level", "world", "radar", "python"]
+#    Output: ["level", "radar"]
 
-print(palindromes(["level", "world", "radar", "python"]))  # Output: ["level", "radar"]
+def find_palindromes(words):
+    return list(filter(lambda w: w == w[::-1], words))
+
+print(find_palindromes(["level", "world", "radar", "python"]))
