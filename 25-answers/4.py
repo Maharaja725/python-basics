742aaca724523ead35265ea900e6f7552b13b6dd
Maharaja <maharaja7251@gmail.com>
2025-06-04 11:22:40 +0000

    latest

--- 25-answers/4.py
+++ 25-answers/4.py
@@ -1,5 +1,10 @@
 # 4. Write a function that takes another function as a callback and applies it to the values of a dictionary, returning a new dictionary.
-def apply_callback(data, callback):
-    return {k: callback(v) for k, v in data.items()}
+#    Input: {"a": 2, "b": 3}, callback: lambda x: x**2
+#    Output: {"a": 4, "b": 9}
 
-print(apply_callback({"a": 2, "b": 3}, lambda x: x**2))  # Output: {"a": 4, "b": 9}
+def apply_callback(data, callback):
+    for key in data:
+        data[key] = callback(data[key])
+    return data
+
+print(apply_callback({"a": 2, "b": 3}, lambda x: x**2))  
