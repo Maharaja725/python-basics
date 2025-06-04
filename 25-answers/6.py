34b0457fe4eab09ccbfb1c50909735dfdaeb152e
Maharaja <maharaja7251@gmail.com>
2025-06-04 11:31:11 +0000

    latest

--- 25-answers/6.py
+++ 25-answers/6.py
@@ -1,4 +1,8 @@
 # 6. Use map to convert a list of integers to their binary string representations.
-to_binary = lambda lst: list(map(bin, lst))
+#    Input: [2, 5, 8]
+#    Output: ['0b10', '0b101', '0b1000']
 
-print(to_binary([2, 5, 8]))  # Output: ['0b10', '0b101', '0b1000']
+def to_binary(lst):
+    return list(map(bin, lst))
+
+print(to_binary([2, 5, 8]))
