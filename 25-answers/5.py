ac0a1d1464fa6bf3e4ad164e3941d4481e02bbbd
Maharaja <maharaja7251@gmail.com>
2025-06-04 11:27:46 +0000

    latest

--- 25-answers/5.py
+++ 25-answers/5.py
@@ -1,5 +1,8 @@
 # 5. Use a lambda function to sort a list of dictionaries by a specific key's value.
-sort_by_key = lambda lst, key: sorted(lst, key=lambda x: x[key])
+#    Input: [{"name": "Alice", "age": 25}, {"name": "Bob", "age": 20}]
+#    Output: [{"name": "Bob", "age": 20}, {"name": "Alice", "age": 25}]
+
+def sort_by_key(lst, key):
+    return sorted(lst, key=lambda x: x[key])
 
 print(sort_by_key([{"name": "Alice", "age": 25}, {"name": "Bob", "age": 20}], "age"))
-# Output: [{"name": "Bob", "age": 20}, {"name": "Alice", "age": 25}]
